f78ee525e1df822c93d89c74b0bbbe1a60b572f5
cj <cj@mosynx.com>
2023-06-30 17:49:59 +0800

    optimization api

--- app/logic/v1/user.py
+++ app/logic/v1/user.py
@@ -1,16 +1,16 @@
 from fastapi import HTTPException
 
-from ...controllers.controllers import sync_catch_controller
+from ...controllers.controllers import catch_controller
 from ...dao import crud
 
 
-@sync_catch_controller
+@catch_controller
 def query_user(uid):
     if uid == 1:
         raise HTTPException(500, 'No permissions')
     return crud.query_user(uid)
 
 
-@sync_catch_controller
+@catch_controller
 def query_user_list():
     return crud.query_user_list()
