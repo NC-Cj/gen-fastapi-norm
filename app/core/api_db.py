bd365141fd0309c70ba26e5d2fb142fc3778e5c8
Cj <2771004336@qq.com>
2023-07-03 23:52:06 +0800

    testing

--- app/core/api_db.py
+++ app/core/api_db.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from typing import Optional, Union
+from typing import Union
 
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from sqlalchemy.orm import Session
@@ -55,29 +55,97 @@ def insert(model, session: Session, data: Union[list, dict], refresh=False):
         return "ok"
 
 
+
 @with_db_session
-def update_many(model, session: Session, data: dict, limit: Optional[int] = 100):
+def update(model, session: Session, filter_by: dict, data: dict):
     try:
-        session.query(model).filter_by(**data.pop('filter', {})).update(data, synchronize_session=False)
+        updated_rows = session.query(model).filter_by(**filter_by).update(data)
         session.commit()
-    except SQLAlchemyError as e:
+    except Exception as e:
         session.rollback()
         raise DatabaseFailure(e) from e
+    else:
+        return updated_rows
+
+@with_db_session
+def delete(model, session: Session, filter_by: dict):
+    try:
+        deleted_rows = session.query(model).filter_by(**filter_by).delete()
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        raise DatabaseFailure(e) from e
+    else:
+        return deleted_rows
+
+@with_db_session
+def upsert(model, session: Session, data: Union[list, dict], unique_columns: list, update_columns=None):
+    try:
+        if isinstance(data, dict):
+            data = [data]
+
+        for entry in data:
+            unique_filter = {key: entry[key] for key in unique_columns}
+            instance = session.query(model).filter_by(**unique_filter).first()
+            if instance:
+                if update_columns:
+                    update_data = {key: entry[key] for key in update_columns}
+                else:
+                    update_data = entry
+                session.query(model).filter_by(**unique_filter).update(update_data)
+            else:
+                instance = model(**entry)
+                session.add(instance)
+        session.commit()
+    except IntegrityError as e:
+        session.rollback()
+        raise DatabaseFailure(e) from e
+    else:
+        return "ok"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @with_db_session
-def update_one(model, session: Session, data: dict, **kwargs):
-    instance = session.query(model).filter_by(**kwargs).first()
-    if not instance:
-        return None
+def update(model, session: Session, data: Union[list, dict], refresh=False, **kwargs):
     try:
-        for key, value in data.items():
-            setattr(instance, key, value)
+        instance = session.query(model)
+        for k, v in kwargs.items():
+            if isinstance(v, (list, tuple)):
+                instance = instance.filter(getattr(model, k).in_(v))
+            else:
+                instance.filter_by(**{k: v})
+
+        print(instance)
+
+        if isinstance(data, (dict, list)):
+            instance.update(data, synchronize_session=False)
+        else:
+            raise TypeError("argument: data, must be a dict or a list of dicts")
         session.commit()
-        return instance
-    except SQLAlchemyError as e:
+    except IntegrityError as e:
         session.rollback()
         raise DatabaseFailure(e) from e
+    else:
+        return instance.all() if refresh else "ok"
 
 
 @with_db_session
