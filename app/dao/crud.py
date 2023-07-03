b0efca95beadbab38ed03e115e1ef3e73c352cd9
Cj <2771004336@qq.com>
2023-07-03 23:52:18 +0800

    testing

--- app/dao/crud.py
+++ app/dao/crud.py
@@ -1,9 +1,25 @@
-from ..models.schema import User
+from typing import List
+
+from ..core import api_db
+from ..models import request
+from ..models.schema import Mxuser
 
 
 def query_user_list():
-    return None
+    return api_db.query_all(Mxuser)
 
 
 def query_user(uid):
-    return uid
+    return api_db.query_first(Mxuser, id=uid)
+
+
+def insert_user(model):
+    return api_db.insert(Mxuser, data=model, refresh=True)
+
+
+def insert_users(model: List[request.User]):
+    return api_db.insert(Mxuser, data=model, refresh=True)
+
+
+def update_user(uid, model: request.User):
+    return api_db.update(Mxuser, id=[uid, 2], username='cj', data={'status': 'fix'}, refresh=True)
