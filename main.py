cafbbbfeba11e41df3806ce389a2b9a3830760f4
cj <cj@mosynx.com>
2023-07-12 14:42:37 +0800

    fixup! add config.json

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from fastapi import FastAPI
 from app import middlewares
 from app.env import env
 from app.routes.v1 import user
+from initialization import go_init
 
-prefix = "/api"
+config = go_init()
 
 """
 Usually your project goes into production, and I don't recommend external access to your documentation
@@ -15,16 +16,16 @@ if env.bool('PROD'):
 else:
     app = FastAPI(
         version='1.2.0',
-        openapi_url=f'{prefix}/openapi.json',
-        docs_url=f'{prefix}/docs'
+        openapi_url=f'{config.prefix}/openapi.json',
+        docs_url=f'{config.prefix}/docs'
     )
 
 middlewares.init_middlewares(app)
 
-app.include_router(user.app, prefix=prefix)
+app.include_router(user.app, prefix=config.prefix)
 
 # And so on for other routes and functions in your application
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True, log_level="warning")
+    uvicorn.run("main:app", **config.server)
