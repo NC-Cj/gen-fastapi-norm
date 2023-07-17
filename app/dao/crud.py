28b09ee184556cb23262ba7dc562f5ecd57ea273
cj <cj@mosynx.com>
2023-07-17 18:32:01 +0800

    修改调用db核心api

--- app/dao/crud.py
+++ app/dao/crud.py
@@ -1,25 +1,31 @@
-from typing import List
-
 from ..core import api_db
-from ..models import request
-from ..models.schema import Mxuser
+from ..models.schema import Domain, Fn
 
 
-def query_user_list():
-    return api_db.query_all(Mxuser)
+# def query_user_list():
+#     return api_db.query_all(Mxuser)
+#
+#
+# def query_user(uid):
+#     return api_db.query_first(Mxuser, id=uid)
+#
+#
+# def insert_user(model):
+#     return api_db.insert(Mxuser, data=model, refresh=True)
+#
+#
+# #
+# # def insert_users(model: List[request.User]):
+# #     return api_db.insert(Mxuser, data=model, refresh=True)
+#
+#
+# def update_user(uid, status):
+#     return api_db.upsert(Mxuser, data=[])
 
 
-def query_user(uid):
-    return api_db.query_first(Mxuser, id=uid)
-
-
-def insert_user(model):
-    return api_db.insert(Mxuser, data=model, refresh=True)
-
-
-def insert_users(model: List[request.User]):
-    return api_db.insert(Mxuser, data=model, refresh=True)
-
-
-def update_user(uid, model: request.User):
-    return api_db.update(Mxuser, id=[uid, 2], username='cj', data={'status': 'fix'}, refresh=True)
+def query_domain_list():
+    return api_db.query(
+        Domain,
+        joins=[Domain.fn],
+        includes=['Fn']
+    )
