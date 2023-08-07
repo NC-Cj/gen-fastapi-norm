17502ee6734badcaedde4c84dd0b9f01f89aa694
Cj <2771004336@qq.com>
2023-08-07 21:13:59 +0800

    models

--- app/models/request.py
+++ app/models/request.py
@@ -1,1 +1,10 @@
 # The request body from your API, usually the body
+
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    username: str
+    status: Optional[str] = "normal"
