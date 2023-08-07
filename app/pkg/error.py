ec90cb1c3f3096e1dc821aef4e587f1afc3b7e54
Cj <2771004336@qq.com>
2023-08-07 20:54:44 +0800

    error.py

--- app/pkg/error.py
+++ app/pkg/error.py
@@ -1,3 +1,6 @@
+from fastapi import HTTPException, status
+
+
 class NoPermissions(Exception):
     def __init__(self, message):
         self.message = message
@@ -11,6 +14,13 @@ class DatabaseFailure(Exception):
 class UnsupportedDataTypeError(Exception):
     def __init__(self, message):
         self.message = message
+
+
+class CustomHTTPException(HTTPException):
+    def __init__(self, message: str, status_code: int = status.HTTP_200_OK):
+        self.message = message
+        self.status_code = status_code
+        super().__init__(status_code=status_code, detail=message)
 
 
 exceptions_to_catch = [NoPermissions, DatabaseFailure]
