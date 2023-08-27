3bd27e411022a13ebb40431483fcba8c585e4c0b
Cj <2771004336@qq.com>
2023-08-27 21:30:45 +0800

    add client.py, add send_traced_http_request methode

---
+++ app/core/client/client.py
@@ -0,0 +1,28 @@
+import requests
+
+from ..logger.log_setup import logger
+from ...pkg.error import CustomHTTPException
+
+
+def send_traced_http_request(traceid,
+                             url,
+                             method='GET',
+                             params=None,
+                             data=None,
+                             headers=None):
+    if headers is None:
+        headers = {}
+
+    headers["X-Request-ID"] = traceid
+
+    try:
+        response = requests.request(method, url, params=params, data=data, headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        logger.error(f"traced request failed: {e}, http code={response.status_code}")
+    else:
+        result = response.json()
+        if result["code"] != 1000:
+            raise CustomHTTPException(f"traceid={traceid}, response={result['code']} {result['msg']}")
+
+        return result
