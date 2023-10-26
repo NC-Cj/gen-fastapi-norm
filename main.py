8baf1b83b6b32921d27419111e9caf623e4eb479
Cj <2771004336@qq.com>
2023-10-27 00:26:16 +0800

    update

--- main.py
+++ main.py
@@ -1,38 +1,58 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from app import middlewares
-from app.env import env
-from app.project_rules import GlobalProjectResponses
-from app.routes.v1 import job
+from app.middlewares import RequestIDMiddleware, ExceptionHandlerMiddleware, LoggingMiddleware
 
-_environment = env.bool("PROD")
-_host = env.str("HOST")
-_port = env.int("PORT")
 
-_project_name = "gen-fastapi-norm-template"
-_prefix = "/api"
-_version = "2.0"
+def get_application():
+    _app = FastAPI(
+        title=_project_name,
+        version=_version,
+        openapi_url=_openapi_url,
+        docs_url=_docs_url,
+        redoc_url=_redoc_url
+    )
 
-# Prohibit the opening of document centers in production environments
-_openapi_url = None if _environment else f"{_prefix}/openapi.json"
-_docs_url = None if _environment else f"{_prefix}/docs"
-_redoc_url = None
+    _app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
-app = FastAPI(
-    title=_project_name,
-    version=_version,
-    openapi_url=_openapi_url,
-    docs_url=_docs_url,
-    redoc_url=_redoc_url
-)
+    _app.add_middleware(RequestIDMiddleware)
+    _app.add_middleware(ExceptionHandlerMiddleware)
+    _app.add_middleware(LoggingMiddleware)
 
-middlewares.init_middlewares(app)
-middlewares.init_exception_handler(app)
+    return _app
 
-app.include_router(job.app, prefix=_prefix, responses=GlobalProjectResponses)
-# app.include_router(you_routes.app, prefix=_prefix, responses=GlobalProjectResponses)
 
-if __name__ == '__main__':
-    import uvicorn
+app = get_application()
 
-    uvicorn.run("main:app", host=_host, port=_port)
+# _environment = env.bool("PROD")
+
+#
+# _project_name = "gen-fastapi-norm-template"
+# _prefix = "/api"
+# _version = "2.0"
+#
+# # Prohibit the opening of document centers in production environments
+# _openapi_url = None if _environment else f"{_prefix}/openapi.json"
+# _docs_url = None if _environment else f"{_prefix}/docs"
+# _redoc_url = None
+#
+# app = FastAPI(
+#     title=_project_name,
+#     version=_version,
+#     openapi_url=_openapi_url,
+#     docs_url=_docs_url,
+#     redoc_url=_redoc_url
+# )
+#
+# middlewares.init_middlewares(app)
+# middlewares.init_exception_handler(app)
+#
+# app.include_router(job.app, prefix=_prefix, responses=GlobalProjectResponses)
+# # app.include_router(you_routes.app, prefix=_prefix, responses=GlobalProjectResponses)
+#
