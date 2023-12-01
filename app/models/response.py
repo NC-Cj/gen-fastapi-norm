bb02dd113be1d1a936936f17c3dd5e317dfbca73
cj <cj@mosynx.com>
2023-12-01 13:15:05 +0800

    更新响应

--- app/models/response.py
+++ app/models/response.py
@@ -3,7 +3,54 @@ from typing import Optional, Any
 from pydantic import BaseModel
 
 
+class ResponseCode:
+    """
+    Response Code.
+
+    Defines the response codes used in the application.
+
+    Attributes:
+        SUCCESS (int): The success response code.
+        FAILURE (int): The failure response code.
+        INTERNAL_ERROR (int): The internal error response code.
+    """
+
+    SUCCESS = 1000
+    FAILURE = 1001
+    INTERNAL_ERROR = 1002
+
+
 class PublicResponse(BaseModel):
-    code: int
+    """
+    Public Response.
+
+    Represents a response object for public cases.
+
+    Attributes:
+        code (int): The response code.
+        data (Optional[Any]): The response data (default: None).
+        msg (Optional[Any]): The response message (default: None).
+    """
+
+    code: ResponseCode
     data: Optional[Any] = None
     msg: Optional[Any] = None
+
+
+class AbnormalResponse(BaseModel):
+    """
+    Abnormal Response.
+
+    Represents a response object for abnormal cases.
+
+    Attributes:
+        code (int): The response code.
+        data (Optional[Any]): The response data (default: None).
+        msg (Optional[Any]): The response message (default: None).
+        request_id (Optional[str]): The request ID (default: None).
+    """
+
+    code: ResponseCode
+    data: Optional[Any] = None
+    msg: Optional[Any] = None
+    request_id: Optional[str] = None
